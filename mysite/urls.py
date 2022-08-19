fa7f3f101156f3cdd2166c6c72a23c9af0abaa2e
yohi <rikou92991@gmail.com>
2022-08-19 13:27:16 +0800

    refactor admin url

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
-    path('django-admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('recipe.urls')),
 ]
 
